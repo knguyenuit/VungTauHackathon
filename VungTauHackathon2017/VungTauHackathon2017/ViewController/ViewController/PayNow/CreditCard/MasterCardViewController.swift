b798d35139925aa96d4abdee557108574245e16a
huyvq225 <huyvq225@gmail.com>
2017-12-07 15:22:31 +0700

    fix payment

--- VungTauHackathon2017/VungTauHackathon2017/ViewController/ViewController/PayNow/CreditCard/MasterCardViewController.swift
+++ VungTauHackathon2017/VungTauHackathon2017/ViewController/ViewController/PayNow/CreditCard/MasterCardViewController.swift
@@ -14,6 +14,11 @@ class MasterCardViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         vCreditCard.layer.cornerRadius = 10
+        ///CreditCard.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.15).cgColor
+//        vCreditCard.layer.borderWidth = 0.5
+        vCreditCard.layer.shadowOffset = CGSize(width: 0, height: 3)
+        vCreditCard.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.15).cgColor
+        vCreditCard.layer.shadowOpacity = 1
         // Do any additional setup after loading the view.
     }
 
--- VungTauHackathon2017/VungTauHackathon2017/ViewController/ViewController/PayNow/PayNowViewController.swift
+++ VungTauHackathon2017/VungTauHackathon2017/ViewController/ViewController/PayNow/PayNowViewController.swift
@@ -12,6 +12,7 @@ import iCarousel
 class PayNowViewController: UIViewController {
 
     @IBOutlet weak var vCarousel: iCarousel!
+    @IBOutlet weak var pcListCards: UIPageControl!
     var screenWidth = UIScreen.main.bounds.size.width
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,24 +41,35 @@ extension PayNowViewController: iCarouselDataSource, iCarouselDelegate {
         var itemView: UIImageView
         //create new view if no view is available for recycling
         if (view == nil) {
-            itemView = UIImageView(frame: CGRect(x: 0, y: 0, width: 319/375 * screenWidth, height: 170))
+            itemView = UIImageView(frame: CGRect(x: 0, y: 0, width: 319/375 * screenWidth, height: 200))
         }
         else {
             itemView = view as! UIImageView
         }
         if index == 0 || index == 2 {
             let view = CreditCardViewController()
-            let a = view.view
-            itemView.addSubview(view.view)
+            if let a = view.view {
+            a.layer.shadowOffset = CGSize(width: 10, height: 10)
+            a.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.2).cgColor
+            a.layer.shadowOpacity = 1
+            a.layer.cornerRadius = 10
+            itemView.addSubview(a)
             return itemView
+            } 
+            
         }
         else {
             let view = MasterCardViewController()
-            let a = view.view
-            itemView.addSubview(view.view)
+            if let a = view.view   {
+            a.layer.shadowOffset = CGSize(width: 10, height: 10)
+            a.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.15).cgColor
+            a.layer.shadowOpacity = 1
+            a.layer.cornerRadius = 10
+            itemView.addSubview(a)
             return itemView
+            }
         }
-        
+        return itemView
     }
     
     
